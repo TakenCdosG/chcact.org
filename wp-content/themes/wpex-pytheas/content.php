7daffda149fc95e40201f9f7f2ad3b99b187eef5
caedlesu <caedlesu@gmail.com>
2015-06-19 17:34:39 -0500

    changed design of news page category (ticket 50)

--- wp-content/themes/wpex-pytheas/content.php
+++ wp-content/themes/wpex-pytheas/content.php
@@ -31,20 +31,11 @@ else {
 	global $wpex_count;
 	$wpex_clr_margin = ( $wpex_count == 1 ) ? 'clr-margin' : NULL; ?>
 	<article id="post-<?php the_ID(); ?>" <?php post_class('blog-entry clr '. $wpex_clr_margin); ?>>
-		<?php if ( has_post_thumbnail() ) { ?>
-			<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark" class="blog-entry-img-link">
-				<img src="<?php echo aq_resize( wp_get_attachment_url( get_post_thumbnail_id() ), wpex_img('blog_entry_width'),  wpex_img('blog_entry_height'),  wpex_img('blog_entry_crop') ) ?>" alt="<?php echo the_title(); ?>" />
-			</a>
-		<?php } ?>
+        <div class="corner-icon"></div>
 		<div class="blog-entry-details">
 			<header><h2><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2></header>
 			<ul class="meta clr">
-				<li><span class="fa fa-clock-o"></span><?php echo get_the_date(); ?></li>
-				<li><span class="fa fa-folder-open"></span><?php the_category(' / '); ?></li>
-				<?php if( comments_open() ) { ?>
-					<li><span class="fa fa-comment"></span><?php comments_popup_link(__('Leave a comment', 'wpex'), __('1 Comment', 'wpex'), __('% Comments', 'wpex'), 'comments-link', __('Comments closed', 'wpex')); ?></li>
-				<?php } ?>
-                <!--<li><span class="fa fa-user"></span><?php //the_author_posts_link(); ?></li>-->
+                <li>by: <?php the_author_posts_link(); ?> · <?php echo get_the_date("j F"); ?></li>
 			</ul><!-- .meta -->
 			<div class="blog-entry-content">
 				<?php
@@ -53,6 +44,11 @@ else {
 				} else {
 					the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'wpex' ) );
 				} ?>
+                <ul class="meta"><li>Categories: <span class="cat-style"><?php the_category(' / '); ?></span></li>
+                    <?php if( comments_open() ) { ?>
+                        <li class="comments-text"><div><span class="comment-link-text"><?php comments_popup_link(__('Leave a comment', 'wpex'), __('1 Comment', 'wpex'), __('% Comments', 'wpex'), 'comments-link', __('Comments closed', 'wpex')); ?></span><span class="news-comments"></span></div></li>
+                    <?php } ?>
+                </ul>
 			</div><!-- /entry-content -->
 		</div><!-- /blog-entry-details -->
 	</article><!-- /blog-entry-entry -->
