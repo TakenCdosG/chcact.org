e2754481720b92e6fbd45c3416634920934b231b
caedlesu <caedlesu@gmail.com>
2015-06-24 09:39:42 -0500

    added rich text functions to WYSIWYG text  editor

--- wp-content/themes/wpex-pytheas/functions.php
+++ wp-content/themes/wpex-pytheas/functions.php
@@ -78,3 +78,32 @@ if( is_admin() ) {
 	require_once( get_template_directory() .'/functions/aqua-resizer.php');
 	
 }
+
+/*
+* ADDING OTHER OPTIONS TINYMCE EDITOR
+*/
+
+function enable_more_buttons($buttons) {
+
+    $buttons[] = 'fontselect';
+    $buttons[] = 'fontsizeselect';
+    $buttons[] = 'styleselect';
+    $buttons[] = 'backcolor';
+    $buttons[] = 'newdocument';
+    $buttons[] = 'cut';
+    $buttons[] = 'copy';
+    $buttons[] = 'charmap';
+    $buttons[] = 'hr';
+    $buttons[] = 'visualaid';
+
+    return $buttons;
+}
+add_filter('mce_buttons_3', 'enable_more_buttons');
+
+add_filter( 'tiny_mce_before_init', 'myformatTinyMCE' );
+function myformatTinyMCE( $in ) {
+
+$in['wordpress_adv_hidden'] = FALSE;
+
+return $in;
+}
