66a15ce00190b9eb6530d7df58286f1a9e984a7e
caedlesu <caedlesu@gmail.com>
2015-07-01 12:15:45 -0500

    fixed links word-wrap and added google analytics code

--- wp-content/themes/wpex-pytheas/header.php
+++ wp-content/themes/wpex-pytheas/header.php
@@ -22,6 +22,22 @@
 	<title><?php wp_title(''); ?><?php if( wp_title('', false) ) { echo ' |'; } ?> <?php bloginfo('name'); ?></title>
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 	<?php wp_head(); ?>
+    <script type="text/javascript">
+
+        var gaq = gaq || [];
+        var pluginUrl =
+            '//www.google-analytics.com/plugins/ga/inpage_linkid.js';
+        _gaq.push(['_require', 'inpage_linkid', pluginUrl]);
+        _gaq.push(['_setAccount', 'UA-1172864-3']);
+        _gaq.push(['_trackPageview']);
+
+        (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+
+    </script>
 </head>
 
 <!-- Begin Body -->
